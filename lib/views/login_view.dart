a332e5026b4b9db64e3b54e0062e693d52cacff6
Adevikki <vadeala@gmail.com>
2022-02-15 04:53:29 +0100

    step-7

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -35,150 +35,185 @@ class _HomePageState extends State<LoginView> {
     final double screenHeight = MediaQuery.of(context).size.height;
     final double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Center(
-          child: Column(
-            children: [
-              TextField(
-                controller: _email,
-                enableSuggestions: false,
-                autocorrect: true,
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  hintText: 'Enter your Email',
-                  labelText: 'Email',
-                  suffixIcon: const Icon(
-                    Icons.person,
-                    color: Colors.blue,
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(
-                      10,
-                    ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      width: 1,
-                      color: Colors.blue.shade300,
-                    ),
-                    borderRadius: BorderRadius.circular(
-                      10,
-                    ),
-                  ),
-                ),
+        appBar: AppBar(
+          title: const Center(
+            child: Text(
+              'Login',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
               ),
-              const SizedBox(
-                height: 15,
-              ),
-              TextField(
-                controller: _password,
-                obscureText: true,
-                enableSuggestions: false,
-                autocorrect: true,
-                decoration: InputDecoration(
-                  hintText: 'Enter your Password here',
-                  labelText: 'Password',
-                  suffixIcon: const Icon(
-                    Icons.lock,
-                    color: Colors.blue,
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(
-                      10,
-                    ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      width: 1,
-                      color: Colors.blue.shade300,
-                    ),
-                    borderRadius: BorderRadius.circular(
-                      10,
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Container(
-                alignment: Alignment.centerRight,
-                child: const Text(
-                  "Forget Password?",
-                  style: TextStyle(
-                    color: Colors.grey,
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-              SizedBox(
-                width: screenWidth * 1,
-                height: screenHeight * 0.05,
-                child: TextButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.blue.shade700,
-                  ),
-                  onPressed: () async {
-                    final email = _email.text;
-                    final password = _password.text;
-                    try {
-                      await FirebaseAuth.instance.signInWithEmailAndPassword(
-                          email: email, password: password);
-                      Navigator.of(context).pushNamedAndRemoveUntil(
-                          '/note_view/', (route) => false);
-                      // devtools.log(userCredential.toString());
-                    } on FirebaseAuthException catch (e) {
-                      if (e.code == 'user-not-found') {
-                        await showErrorDialog(
-                          context,
-                          'User not found!',
-                        );
-                        // print('user not found');
-                      } else if (e.code == 'wrong-password') {
-                        await showErrorDialog(
-                          context,
-                          'Wrong Password',
-                        );
-                        // print(e.code);
-                      } else {
-                        await showErrorDialog(
-                          context,
-                          'Error: ${e.code}',
-                        );
-                      }
-                    } catch (e) {
-                      await showErrorDialog(
-                        context,
-                        e.toString(),
-                      );
-                    }
-                    // on SocketException catch (_) {
-                    //   print('Check your connection guy no stress me!');
-                    // }
-                  },
-                  child: const Text(
-                    'Login',
-                    style: TextStyle(fontSize: 20, color: Colors.white),
-                  ),
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context)
-                      .pushNamedAndRemoveUntil(registerRoute, (route) => false);
-                },
-                child: const Text('Not registered? Register here!'),
-              ),
-            ],
+            ),
           ),
         ),
-      ),
-    );
+        body: Container(
+          margin: const EdgeInsets.only(
+            left: 25,
+            right: 25,
+            top: 50,
+          ),
+          height: screenHeight * 2.35,
+          width: screenWidth * 1,
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  const Text(
+                    'Welcome!',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextField(
+                    controller: _email,
+                    enableSuggestions: false,
+                    autocorrect: true,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      hintText: 'Enter your Email',
+                      labelText: 'Email',
+                      suffixIcon: const Icon(
+                        Icons.person,
+                        color: Colors.blue,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(
+                          10,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 1,
+                          color: Colors.blue.shade300,
+                        ),
+                        borderRadius: BorderRadius.circular(
+                          10,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  TextField(
+                    controller: _password,
+                    obscureText: true,
+                    enableSuggestions: false,
+                    autocorrect: true,
+                    decoration: InputDecoration(
+                      hintText: 'Enter your Password here',
+                      labelText: 'Password',
+                      suffixIcon: const Icon(
+                        Icons.lock,
+                        color: Colors.blue,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(
+                          10,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 1,
+                          color: Colors.blue.shade300,
+                        ),
+                        borderRadius: BorderRadius.circular(
+                          10,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    alignment: Alignment.centerRight,
+                    child: const Text(
+                      "Forget Password?",
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  SizedBox(
+                    width: screenWidth * 1,
+                    height: screenHeight * 0.05,
+                    child: TextButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.blue.shade700,
+                      ),
+                      onPressed: () async {
+                        final email = _email.text;
+                        final password = _password.text;
+                        try {
+                          await FirebaseAuth.instance
+                              .signInWithEmailAndPassword(
+                                  email: email, password: password);
+                          Navigator.of(context).pushNamedAndRemoveUntil(
+                              noteViewRoute, (route) => false);
+                          // devtools.log(userCredential.toString());
+                        } on FirebaseAuthException catch (e) {
+                          if (e.code == 'user-not-found') {
+                            await showErrorDialog(
+                              context,
+                              'User not found!',
+                            );
+                            // print('user not found');
+                          } else if (e.code == 'wrong-password') {
+                            await showErrorDialog(
+                              context,
+                              'Wrong Password',
+                            );
+                            // print(e.code);
+                          } else {
+                            await showErrorDialog(
+                              context,
+                              'Error: ${e.code}',
+                            );
+                          }
+                        } catch (e) {
+                          await showErrorDialog(
+                            context,
+                            e.toString(),
+                          );
+                        }
+                        // on SocketException catch (_) {
+                        //   print('Check your connection guy no stress me!');
+                        // }
+                      },
+                      child: const Text(
+                        'Login',
+                        style: TextStyle(fontSize: 20, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pushNamedAndRemoveUntil(
+                          registerRoute, (route) => false);
+                    },
+                    child: const Text(
+                      'Not registered? Register here!',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/constants/routes.dart';
+import 'package:mynotes/utilities/show_error_dialog.dart';
 
 class RegisterView extends StatefulWidget {
   const RegisterView({Key? key}) : super(key: key);
@@ -58,18 +59,40 @@ class _RegisterViewState extends State<RegisterView> {
                   final email = _email.text;
                   final password = _password.text;
                   try {
-                    final userCredential = await FirebaseAuth.instance
-                        .createUserWithEmailAndPassword(
-                            email: email, password: password);
-                    print(userCredential);
+                    await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                      email: email,
+                      password: password,
+                    );
+                    final user = FirebaseAuth.instance.currentUser;
+                    user?.sendEmailVerification();
+                    Navigator.of(context).pushNamed(verifyEmailRoute);
                   } on FirebaseAuthException catch (e) {
                     if (e.code == 'weak-password') {
-                      print('weak password');
+                      await showErrorDialog(
+                        context,
+                        'Weak Password',
+                      );
                     } else if (e.code == 'email-already-in-use') {
-                      print('Email already in use');
+                      await showErrorDialog(
+                        context,
+                        'Email already in use',
+                      );
                     } else if (e.code == 'invalid-email') {
-                      print('Invalid email');
+                      await showErrorDialog(
+                        context,
+                        'This is an invalid Email address',
+                      );
+                    } else {
+                      await showErrorDialog(
+                        context,
+                        'Error: ${e.code}',
+                      );
                     }
+                  } catch (e) {
+                    await showErrorDialog(
+                      context,
+                      e.toString(),
+                    );
                   }
                 },
                 child: const Text('Register'),
